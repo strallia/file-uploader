8301c659f00b955e9b012dc47f5b51876121f9f2
Strallia Chao <134454347+strallia@users.noreply.github.com>
2025-01-06 22:51:28 -0800

    feat: setup Prisma sessions

---
+++ app.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+const passport = require("./configs/passport");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+/**
+ * Configure Prisma sessions
+ */
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
+app.use(passport.session());
+app.use((req, res, next) => {
+  // Store user details in locals for easy access in view templates
+  res.locals.currentUser = req.user;
+  next();
+});
+
+const PORT = process.env.port || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server started on port ${PORT}`);
+});
