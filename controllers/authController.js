184e200737d77d5da4c320feeba9652f4587c54c
Strallia Chao <134454347+strallia@users.noreply.github.com>
2025-01-07 21:13:16 -0800

    feat: add server-side validations to signup form

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,8 +6,37 @@ const getSignUpForm = (req, res) => {
   res.render("signup");
 };
 
-const validateSignupForm = () => {};
+const validateSignupForm = [
+  [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Please enter valid email address (e.g. first@last.com)"),
+    body("password")
+      .notEmpty()
+      .withMessage("Password is required")
+      .matches(/[a-z]/)
+      .withMessage("Password must contain at least one lowercase letter")
+      .matches(/[A-Z]/)
+      .withMessage("Password must contain at least one uppercase letter")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+    body("confirm-password")
+      .notEmpty()
+      .withMessage("Re-enter your password")
+      .custom((value, { req }) => value === req.body.password)
+      .withMessage("Passwords do not match"),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("signup", { errors: errors.array(), ...req.body });
+    }
+    next();
+  },
+];
 
-const postCreateUser = () => {};
+const postCreateUser = (req, res) => {};
 
 module.exports = { getSignUpForm, validateSignupForm, postCreateUser };
